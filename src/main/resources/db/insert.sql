6c09022cc03e6a48c461ad40daea56492c137cd9
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-28 00:22:43 +0100

    insert query

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,29 @@
+-- set foreign_key_checks = 0;
+
+-- truncate table product;
+-- truncate table feedback;
+
+
+insert into student (id, first_name, last_name,gender)
+values(110,'Damilare', 'Jolayemi', 'MALE'),
+      (111,'John', 'Doe', 'MALE'),
+      (112,'Jane', 'Doe', 'FEMALE'),
+      (113,'Felicia', 'Parker', 'FEMALE');
+
+
+insert into instructor (id, first_name, last_name, specialization, bio)
+values(114,'Damilare', 'Jolayemi', 'Software Engineering', 'Lorem Ipsum'),
+      (115,'John', 'Doe', 'Teaching', 'Lorem Ipsum'),
+      (116,'Jane', 'Doe', 'Diving', 'Lorem ipsum'),
+      (117,'Felicia', 'Parker', 'Content creation', 'lorem ipsum');
+
+insert into learning_party (id, email, password,enabled)
+values(118,'dummy@gmail.com', 'password123', true),
+      (119,'dummy1@gmail.com', 'password123', false);
+
+insert into course (id, title, is_published,duration, description, language)
+values(120,'DSA', true, '3 months', 'lorem ipsum', 'French'),
+      (121,'Introduction to programming', false, '3 weeks', 'lorem ipsum', 'English');
+
+
+-- set foreign_key_checks = 1;
