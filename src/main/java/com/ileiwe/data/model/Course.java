a220b48dea39fc3c1e60f4827df0de3cc559cd97
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-28 00:21:28 +0100

    course

---
+++ src/main/java/com/ileiwe/data/model/Course.java
@@ -0,0 +1,36 @@
+package com.ileiwe.data.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class Course{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @CreationTimestamp
+    private LocalDate dateCreated;
+    private LocalDate dataPublished;
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+    private boolean isPublished;
+    private String duration;
+    @Column(length = 1000)
+    private String description;
+    @ElementCollection
+    private List<String> imageUrls;
+    private String language;
+    @ManyToOne
+    private Instructor instructor;
+    @ManyToMany
+    private List<Student> students;
+}
