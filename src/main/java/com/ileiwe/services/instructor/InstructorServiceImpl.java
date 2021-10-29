aedccdb478e2ebe1f83a0bb4962bc3991980677a
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-29 17:51:49 +0100

    services

---
+++ src/main/java/com/ileiwe/services/instructor/InstructorServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ileiwe.services.instructor;
+
+
+import com.ileiwe.data.model.Authority;
+import com.ileiwe.data.model.Instructor;
+import com.ileiwe.data.model.LearningParty;
+import com.ileiwe.data.model.Role;
+import com.ileiwe.data.model.dto.InstructorPartyDto;
+import com.ileiwe.data.repository.InstructorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.ileiwe.data.model.Role.ROLE_INSTRUCTOR;
+
+@Service
+public class InstructorServiceImpl implements InstructorService{
+
+    @Autowired
+    InstructorRepository instructorRepository;
+
+    @Override
+    public Instructor saveInstructor(InstructorPartyDto instructorPartyDto) {
+        if(instructorPartyDto == null){
+            throw new IllegalArgumentException("Instructor cannot be null");
+        }
+        LearningParty learningParty = new LearningParty(instructorPartyDto.getEmail(), instructorPartyDto.getPassword(), new Authority(ROLE_INSTRUCTOR));
+        Instructor instructor = Instructor.builder()
+                                        .firstName(instructorPartyDto.getFirstName())
+                                        .lastName(instructorPartyDto.getLastName())
+                                        .learningParty(learningParty)
+                                        .build();
+
+
+
+       return instructorRepository.save(instructor);
+
+    }
+}
