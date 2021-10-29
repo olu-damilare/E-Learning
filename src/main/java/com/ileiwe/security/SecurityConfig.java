e429925167c66099c8b87d8b84c5b1f47fa0c86b
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-29 17:21:47 +0100

    security config

---
+++ src/main/java/com/ileiwe/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.ileiwe.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final LearningPartyServiceImpl userDetails;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(LearningPartyServiceImpl userDetails, PasswordEncoder passwordEncoder) {
+        this.userDetails = userDetails;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public SecurityConfig(boolean disableDefaults, LearningPartyServiceImpl userDetails, PasswordEncoder passwordEncoder) {
+        super(disableDefaults);
+        this.userDetails = userDetails;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .addFilter(new JWTAuthenticationFilter())
+                    .addFilter(new JWTAuthorizationFilter(authenticationManager()));
+
+
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetails)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+}
