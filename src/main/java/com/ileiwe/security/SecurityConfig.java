c49848e5d45052b19556de7043f2255b8a314c33
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-01 10:57:32 +0100

    security config

--- src/main/java/com/ileiwe/security/SecurityConfig.java
+++ src/main/java/com/ileiwe/security/SecurityConfig.java
@@ -2,14 +2,13 @@ package com.ileiwe.security;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -25,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
+
     public SecurityConfig(boolean disableDefaults, LearningPartyServiceImpl userDetails, PasswordEncoder passwordEncoder) {
         super(disableDefaults);
         this.userDetails = userDetails;
@@ -33,11 +33,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
+            http.csrf()
+                    .disable()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST,"/api/instructor")
+                    .permitAll()
                     .anyRequest()
                     .authenticated()
                     .and()
-                    .addFilter(new JWTAuthenticationFilter())
+                    .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                     .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                     .exceptionHandling()
                     .and()
@@ -59,8 +63,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(10);
-    }
+
 }
