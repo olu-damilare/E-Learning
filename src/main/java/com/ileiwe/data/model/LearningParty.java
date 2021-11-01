46510d444ae0c25b57da0001103c123bed35ce19
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-01 11:01:16 +0100

    update

--- src/main/java/com/ileiwe/data/model/LearningParty.java
+++ src/main/java/com/ileiwe/data/model/LearningParty.java
@@ -1,28 +1,54 @@
 package com.ileiwe.data.model;
 
-import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-
-@Data
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LearningParty {
 
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull @NotEmpty @NotBlank
     @Column(unique = true, nullable = false)
     private String email;
     @Column(nullable = false)
+    @NotNull @NotEmpty @NotBlank
     private String password;
     private boolean enabled;
     @CreationTimestamp
-    private LocalDate dateCreated;
-    @OneToMany
+    private LocalDateTime dateCreated;
+    @OneToMany(cascade = CascadeType.PERSIST)
     private List<Authority> authorities;
+
+    public LearningParty(String email, String password, Authority authority){
+        this.email = email;
+        this.password = password;
+        addAuthority ( authority );
+        this.enabled = false;
+
+    }
+    public void addAuthority(Authority authority){
+        if(this.authorities == null){
+            this.authorities = new ArrayList<> ();
+        }
+        this.authorities.add ( authority );
+    }
+
 }
