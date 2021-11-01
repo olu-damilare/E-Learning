8614f75a86a09e9ec571fb242b47482890a9b244
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-01 10:56:41 +0100

    instructor party dto

---
+++ src/main/java/com/ileiwe/data/model/dto/InstructorPartyDto.java
@@ -0,0 +1,12 @@
+package com.ileiwe.data.model.dto;
+
+import lombok.Data;
+
+@Data
+public class InstructorPartyDto {
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
