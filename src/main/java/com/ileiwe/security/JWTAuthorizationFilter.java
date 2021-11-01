761c091ae5531baf25cb2701259039cd6803ad32
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-01 13:14:44 +0100

    update

--- src/main/java/com/ileiwe/security/JWTAuthorizationFilter.java
+++ src/main/java/com/ileiwe/security/JWTAuthorizationFilter.java
@@ -3,10 +3,12 @@ package com.ileiwe.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -20,6 +22,9 @@ import static com.ileiwe.security.SecurityConstants.*;
 
 @Slf4j
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+//    @Autowired
+//    SecurityConstants constants;
 
     public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
