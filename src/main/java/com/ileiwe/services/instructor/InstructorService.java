a2930b7cf505aae72d1cace9274246ca65ce1cfb
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-02 22:49:12 +0100

    instructor service

--- src/main/java/com/ileiwe/services/instructor/InstructorService.java
+++ src/main/java/com/ileiwe/services/instructor/InstructorService.java
@@ -1,11 +1,15 @@
 package com.ileiwe.services.instructor;
 
 
+import com.ileiwe.data.model.Course;
 import com.ileiwe.data.model.Instructor;
+import com.ileiwe.data.model.dto.CourseDto;
 import com.ileiwe.data.model.dto.InstructorPartyDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface InstructorService {
 
     Instructor saveInstructor(InstructorPartyDto instructor);
+    Course createCourse(CourseDto courseDto, MultipartFile courseImage);
 
 }
