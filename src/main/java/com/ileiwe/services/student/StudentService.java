55382de0eebcd955c4121ff016be1961a847b141
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-02 22:53:06 +0100

    student service

--- src/main/java/com/ileiwe/services/student/StudentService.java
+++ src/main/java/com/ileiwe/services/student/StudentService.java
@@ -1,12 +1,12 @@
-package com.ileiwe.services.student;
-
-
-import com.ileiwe.data.model.Student;
-import com.ileiwe.data.model.dto.StudentPartyDto;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface StudentService {
-
-    Student registerStudent(StudentPartyDto studentPartyDto);
-}
+//package com.ileiwe.services.student;
+//
+//
+//import com.ileiwe.data.model.Student;
+//import com.ileiwe.data.model.dto.StudentPartyDto;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public interface StudentService {
+//
+//    Student registerStudent(StudentPartyDto studentPartyDto);
+//}
