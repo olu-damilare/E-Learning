9ab85dcf909257c4f5fd06efd4d395bdfa01697d
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-04 05:58:56 +0100

    keys

--- src/main/java/com/ileiwe/security/SecurityConstants.java
+++ src/main/java/com/ileiwe/security/SecurityConstants.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class SecurityConstants {
 
-    @Value("${secret}")
-    public static String SECRET;
-    @Value("${expiration.time}")
-    public static long EXPIRATION_TIME; //10 DAYS
-    public static final String TOKEN_PREFIX = "Bearer";
+//    @Value("${secret}")
+    public static String SECRET = "864000000";
+//    @Value("${expiration.time}")
+    public static long EXPIRATION_TIME = 864000000; //10 DAYS
+    public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
