37063319449427db4ea7296232173c7a56771bdb
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-04 06:00:03 +0100

    course update endpoint added

--- src/main/java/com/ileiwe/controller/InstructorController.java
+++ src/main/java/com/ileiwe/controller/InstructorController.java
@@ -1,14 +1,13 @@
 package com.ileiwe.controller;
 
 
+import com.ileiwe.data.model.dto.CourseDto;
 import com.ileiwe.data.model.dto.InstructorPartyDto;
 import com.ileiwe.services.instructor.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController()
 @RequestMapping("/api/instructor")
@@ -25,4 +24,29 @@ public class InstructorController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    @PostMapping("/course")
+    public ResponseEntity<?> createCourse(@RequestBody CourseDto courseDto, @RequestBody MultipartFile courseImage){
+        try {
+            return ResponseEntity.ok().body(instructorService.createCourse(courseDto, courseImage));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/course/{id}")
+    public ResponseEntity<?> updateCourse(@RequestBody CourseDto courseDto,
+                                          @PathVariable Long id,
+                                          @RequestBody MultipartFile courseImage){
+        try {
+            return ResponseEntity.ok().body(instructorService.updateCourse(courseDto, id, courseImage));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+
+
+
 }
