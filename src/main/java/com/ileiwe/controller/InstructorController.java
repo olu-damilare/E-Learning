179460303fb4ad81dc84338cc596d89b6bd12e34
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-05 00:18:33 +0100

    get instructor courses endpoint

--- src/main/java/com/ileiwe/controller/InstructorController.java
+++ src/main/java/com/ileiwe/controller/InstructorController.java
@@ -25,18 +25,14 @@ public class InstructorController {
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    @GetMapping("/{username}/courses")
+    public ResponseEntity<?> getInstructorCourses(@PathVariable("username") String username){
+        try {
+            return ResponseEntity.ok().body(instructorService.getInstructorCourses(username));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 
 
