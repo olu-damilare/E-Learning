26ba3f01d0f02fe632f5f744674d84dcc1110a56
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-04 05:57:11 +0100

    additional test

--- src/test/java/com/ileiwe/services/InstructorServicesImplTest.java
+++ src/test/java/com/ileiwe/services/InstructorServicesImplTest.java
@@ -2,6 +2,7 @@ package com.ileiwe.services;
 
 
 import com.ileiwe.data.model.Authority;
+import com.ileiwe.data.model.Course;
 import com.ileiwe.data.model.Instructor;
 import com.ileiwe.data.model.LearningParty;
 import com.ileiwe.data.model.dto.CourseDto;
@@ -57,13 +58,71 @@ public class InstructorServicesImplTest {
                 .build();
 
 
-        instructorService.createCourse(course, null);
+       Course savedCourse = instructorService.createCourse(course, null);
+
+        log.info("saved course is --> {}", savedCourse);
+
+        assertThat(savedCourse.getId()).isNotNull();
 
 
         log.info("Instructor after creating course --> {}", savedInstructor);
 
         assertThat(savedInstructor.getCourses().get(0).getTitle()).isEqualTo(course.getTitle());
         assertThat(course.getInstructorUsername()).isEqualTo(instructor.getLearningParty().getEmail());
+
+    }
+
+
+    @Test
+    void testToUpdateCourse(){
+        LearningParty user = new LearningParty("trainer@ileiwe.com", "Password123", new Authority(ROLE_INSTRUCTOR));
+
+        Instructor instructor =
+                Instructor.builder()
+                        .firstName("John")
+                        .lastName("Doe")
+                        .learningParty(user)
+                        .build();
+
+        instructorRepository.save(instructor);
+        assertThat(instructor.getId()).isNotNull();
+        assertThat(instructor.getLearningParty().getId()).isNotNull();
+
+        log.info("Instructor after saving to db --> {}", instructor);
+
+        Instructor savedInstructor = instructorRepository.findById(instructor.getId()).orElse(null);
+        assertThat(savedInstructor).isNotNull();
+
+        CourseDto course = CourseDto.builder()
+                .title("How to program in Java")
+                .instructorUsername(savedInstructor.getLearningParty().getEmail())
+                .description("lorem ipsum")
+                .language("English")
+                .duration("28 hours of recorded video")
+                .build();
+
+
+        Course savedCourse = instructorService.createCourse(course, null);
+
+        log.info("saved course is --> {}", savedCourse);
+
+        assertThat(savedCourse.getId()).isNotNull();
+
+        assertThat(savedCourse.getDuration()).isEqualTo(course.getDuration());
+
+        CourseDto courseUpdateDto = CourseDto.builder()
+                                            .duration("4 weeks of recorded video")
+                                            .build();
+
+        log.info("Instructor before updating course --> {}", instructor);
+        Course updatedCourse = instructorService.updateCourse(courseUpdateDto, savedCourse.getId(), null);
+        savedInstructor = instructorRepository.findById(instructor.getId()).get();
+
+        log.info("Instructor after updating course --> {}", instructor);
+        assertThat(savedInstructor.getCourses().get(0).getDuration()).isEqualTo(courseUpdateDto.getDuration());
+        assertThat(updatedCourse.getId()).isEqualTo(savedInstructor.getCourses().get(0).getId());
+        assertThat(updatedCourse.getDuration()).isEqualTo(savedInstructor.getCourses().get(0).getDuration());
+
 
     }
 }
