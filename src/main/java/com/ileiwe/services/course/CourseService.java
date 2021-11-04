5319feb49610e683f129b847df350213d9784463
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-04 22:58:11 +0100

    get course

--- src/main/java/com/ileiwe/services/course/CourseService.java
+++ src/main/java/com/ileiwe/services/course/CourseService.java
@@ -3,10 +3,16 @@ package com.ileiwe.services.course;
 import com.ileiwe.data.model.Course;
 import com.ileiwe.data.model.dto.CourseDto;
 
+import java.util.List;
+
 public interface CourseService {
 
     Course saveCourse(CourseDto courseDto);
 
     Course findById(Long id);
     Course updateCourse(CourseDto courseUpdateDto, Long courseId);
+
+    void deleteCourse(Long courseId);
+
+    List<Course> getCoursesByTitle(String title);
 }
