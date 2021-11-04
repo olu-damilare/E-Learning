4be66fb446347d60eab5a4863e610139af2382b1
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-04 06:00:37 +0100

    configured ignore null values

--- src/main/java/com/ileiwe/configurations/ModelMapperConfig.java
+++ src/main/java/com/ileiwe/configurations/ModelMapperConfig.java
@@ -9,7 +9,10 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+
+        return modelMapper;
     }
 
 }
