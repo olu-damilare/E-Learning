48580a66aa9d28c8fb49cd7f47ffd17523589fcc
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-05 22:59:46 +0100

    additional tests

--- src/test/java/com/ileiwe/services/StudentServiceImplTests.java
+++ src/test/java/com/ileiwe/services/StudentServiceImplTests.java
@@ -6,6 +6,7 @@ import com.ileiwe.data.model.Student;
 import com.ileiwe.services.course.CourseService;
 import com.ileiwe.services.student.StudentService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,14 +28,50 @@ public class StudentServiceImplTests {
     @Autowired
     CourseService courseService;
 
+    Student student;
+    Course course;
+
+    @BeforeEach
+    void setUp(){
+        student = studentService.findStudentById(110L);
+
+
+        course = courseService.findById(120L);
+
+    }
+
     @Test
     void testThatStudentCanEnrollForCourse(){
-        Student student = studentService.findStudentById(110L);
+
         assertThat(student).isNotNull();
         assertThat(student.getCourses().size()).isEqualTo(0);
         log.info("Student before enrolling for course --> {}", student);
 
-        Course course = courseService.findById(120L);
+        assertThat(course).isNotNull();
+        assertThat(course.getStudents().size()).isEqualTo(0);
+        log.info("Course before student enrollment --> {}", course);
+
+
+        studentService.enroll(student.getId(), course.getId());
+        log.info("Student after enrolling for course --> {}", student);
+        log.info("Course after student enrollment --> {}", course);
+
+        assertThat(student.getCourses()).isNotNull();
+        assertThat(student.getCourses().get(0).getId()).isEqualTo(course.getId());
+
+        assertThat(course.getStudents()).isNotNull();
+        assertThat(course.getStudents().get(0).getId()).isEqualTo(student.getId());
+
+
+    }
+
+    @Test
+    void testThatStudentCanUnEnrollForCourse(){
+
+        assertThat(student).isNotNull();
+        assertThat(student.getCourses().size()).isEqualTo(0);
+        log.info("Student before enrolling for course --> {}", student);
+
         assertThat(course).isNotNull();
         assertThat(course.getStudents().size()).isEqualTo(0);
         log.info("Course before student enrollment --> {}", course);
@@ -49,8 +86,40 @@ public class StudentServiceImplTests {
         assertThat(course.getStudents()).isNotNull();
         assertThat(course.getStudents().get(0).getId()).isEqualTo(student.getId());
 
+        studentService.unEnroll(student.getId(), course.getId());
 
-
+        assertThat(student.getCourses().size()).isEqualTo(0);
+        assertThat(course.getStudents().size()).isEqualTo(0);
+        log.info("Student after un-enrolling for course --> {}", student);
 
     }
+
+    @Test
+    void testThatStudentCannotEnrollForSameCourseMultipleTimes(){
+
+        assertThat(student).isNotNull();
+        assertThat(student.getCourses().size()).isEqualTo(0);
+        log.info("Student before enrolling for course --> {}", student);
+
+        assertThat(course).isNotNull();
+        assertThat(course.getStudents().size()).isEqualTo(0);
+        log.info("Course before student enrollment --> {}", course);
+
+        studentService.enroll(student.getId(), course.getId());
+        log.info("Student after enrolling for course --> {}", student);
+        log.info("Course after student enrollment --> {}", course);
+
+        assertThat(student.getCourses()).isNotNull();
+        assertThat(student.getCourses().size()).isEqualTo(1);
+
+        assertThat(course.getStudents()).isNotNull();
+        assertThat(course.getStudents().size()).isEqualTo(1);
+
+        studentService.enroll(student.getId(), course.getId());
+
+        assertThat(student.getCourses().size()).isEqualTo(1);
+        assertThat(course.getStudents().size()).isEqualTo(1);
+    }
+
+
 }
