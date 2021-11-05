a23147a3b846b17cf074df1a87b5d6f91822e9a9
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-05 16:19:22 +0100

    course enrollment endpoint

--- src/main/java/com/ileiwe/controller/StudentController.java
+++ src/main/java/com/ileiwe/controller/StudentController.java
@@ -1,14 +1,12 @@
 package com.ileiwe.controller;
 
 
+import com.ileiwe.data.model.Student;
 import com.ileiwe.data.model.dto.StudentPartyDto;
 import com.ileiwe.services.student.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/student")
@@ -26,5 +24,17 @@ public class StudentController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    @PostMapping("/{username}/enroll")
+    public ResponseEntity<?> enrollForCourse(@PathVariable("username") String username, @RequestParam Long courseId){
+        try {
+            Student student = studentService.findStudentByUsername(username);
+            studentService.enroll(student.getId(), courseId);
+            return ResponseEntity.noContent().build();
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 
 }
