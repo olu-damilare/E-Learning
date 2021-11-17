08df3c67b48a7f3123fbb4f6945049beb55dc72d
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-17 01:57:05 +0100

    update

--- src/main/java/com/ileiwe/services/instructor/InstructorService.java
+++ src/main/java/com/ileiwe/services/instructor/InstructorService.java
@@ -3,7 +3,8 @@ package com.ileiwe.services.instructor;
 
 import com.ileiwe.data.model.Course;
 import com.ileiwe.data.model.Instructor;
-import com.ileiwe.data.model.dto.CourseDto;
+import com.ileiwe.data.model.dto.CourseCreateDto;
+import com.ileiwe.data.model.dto.InstructorDetailsDto;
 import com.ileiwe.data.model.dto.InstructorPartyDto;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,11 +14,11 @@ import java.util.List;
 public interface InstructorService {
 
     Instructor saveInstructor(InstructorPartyDto instructor);
-    Course createCourse(CourseDto courseDto, MultipartFile courseImage) throws IOException;
+    Course createCourse(CourseCreateDto courseDto, MultipartFile courseImage) throws IOException;
 
-    Course updateCourse(CourseDto courseUpdateDto, Long courseId, MultipartFile courseImage);
+    Course updateCourse(CourseCreateDto courseUpdateDto, Long courseId, MultipartFile courseImage);
 
-    Instructor findByUsername(String username);
+    InstructorDetailsDto findByUsername(String username);
 
     void deleteCourse(String instructorUsername, Long courseId);
 
@@ -25,5 +26,7 @@ public interface InstructorService {
 
     List<Course> getInstructorCourses(String instructorUsername);
 
-    Instructor enableInstructor(String instructorUsername);
+    Instructor enableInstructor(Long instructorId);
+
+    List<InstructorDetailsDto> getAllInstructors();
 }
